b25eea8bb53b8c1d3ba390fcd5e5012d8bb424cc
Bacze12 <brunocc.0205@gmail.com>
2025-01-13 03:22:10 -0300

    Add additional configurations to Jest
    * Add `transformIgnorePatterns` to allow transformation of `@nestjs/common`
    * Set `rootDir` to `src` to ensure Jest searches for test files in the `src` folder
    * Add `testRegex` to only execute test files with `.spec.ts` extension
    * Add `collectCoverageFrom` to include files for coverage
    * Set `coverageDirectory` to `../coverage` for coverage report output

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,13 @@ module.exports = {
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!@nestjs/common)', // Permitir la transformación de @nestjs/common
+  ],
+  rootDir: 'src', // Asegura que Jest busque archivos de prueba en la carpeta src
+  testRegex: '.*\\.spec\\.ts$', // Solo ejecuta archivos de prueba con esta extensión
+  collectCoverageFrom: [
+    '**/*.(t|j)s', // Incluir archivos para la cobertura
+  ],
+  coverageDirectory: '../coverage', // Salida de informes de cobertura
 };
