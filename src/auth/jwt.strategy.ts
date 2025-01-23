76097a842cafca0bb75c413ab7e8cda37a2c4070
Bacze12 <brunocc.0205@gmail.com>
2025-01-23 12:09:46 -0300

    Enhance JWT strategy to validate user existence and active status during authentication

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,15 +2,18 @@ import { Injectable, UnauthorizedException, Logger } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { jwtConfig } from '../config/jwt.confg';
+import { UsersService } from '../modules/users/users.service'; // Importa tu servicio de usuarios
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly usersService: UsersService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: jwtConfig.secret,
     });
   }
+
   private readonly logger = new Logger(JwtStrategy.name);
 
   async validate(payload: any) {
@@ -20,13 +23,28 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       throw new UnauthorizedException('Token inválido - Falta tenantId');
     }
 
-    const user = {
-      tenantId: payload.tenantId,
-      email: payload.email,
-      role: payload.role || 'USER', // Distingue entre USER y ADMIN
-    };
+    // Busca al usuario en la base de datos para validar isActive
+    const user = await this.usersService.findById(payload.sub, payload.tenantId); // `sub` suele ser el ID del usuario en el payload JWT
 
-    this.logger.log('Usuario validado:', user);
-    return user;
+    if (!user) {
+      throw new UnauthorizedException('Usuario no encontrado.');
+    }
+
+    if (!user.isActive) {
+      throw new UnauthorizedException('El usuario está inactivo.');
+    }
+
+    this.logger.log('Usuario validado y activo:', {
+      tenantId: user.tenantId,
+      email: user.email,
+      role: user.role,
+    });
+
+    return {
+      id: user._id,
+      tenantId: user.tenantId,
+      email: user.email,
+      role: user.role || 'USER', // Distingue entre USER y ADMIN
+    };
   }
 }
