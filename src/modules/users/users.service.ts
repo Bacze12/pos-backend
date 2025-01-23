987fdb2ae7b55181bf538abc1b43b3001065bf81
Bacze12 <brunocc.0205@gmail.com>
2025-01-23 11:35:45 -0300

    Remove unused Roles decorator from UsersService

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -9,7 +9,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './users.schema';
 import { hashPassword } from '../../middleware/crypto.middleware';
-import { Roles } from '@/common/decorator/roles.decorator';
 
 @Injectable()
 export class UsersService {
@@ -98,7 +97,6 @@ export class UsersService {
     this.logger.log(`Usuario eliminado con éxito.`);
     return { message: 'Usuario eliminado con éxito' };
   }
-  @Roles('ADMIN')
   async active(id: string, tenantId: string, isActive: boolean) {
     // Find user without checking current isActive status
     const user = await this.userModel
