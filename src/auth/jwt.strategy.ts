ebdec6249c9e36183c0c680a088938ee9236ee62
Bacze12 <brunocc.0205@gmail.com>
2025-01-23 14:52:11 -0300

    Refactor JwtStrategy to remove user service dependency and simplify user validation logic

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,18 +2,15 @@ import { Injectable, UnauthorizedException, Logger } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { jwtConfig } from '../config/jwt.confg';
-import { UsersService } from '../modules/users/users.service'; // Importa tu servicio de usuarios
-
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly usersService: UsersService) {
+  constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: jwtConfig.secret,
     });
   }
-
   private readonly logger = new Logger(JwtStrategy.name);
 
   async validate(payload: any) {
@@ -23,28 +20,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       throw new UnauthorizedException('Token inválido - Falta tenantId');
     }
 
-    // Busca al usuario en la base de datos para validar isActive
-    const user = await this.usersService.findById(payload.sub, payload.tenantId); // `sub` suele ser el ID del usuario en el payload JWT
-
-    if (!user) {
-      throw new UnauthorizedException('Usuario no encontrado.');
-    }
-
-    if (!user.isActive) {
-      throw new UnauthorizedException('El usuario está inactivo.');
-    }
-
-    this.logger.log('Usuario validado y activo:', {
-      tenantId: user.tenantId,
-      email: user.email,
-      role: user.role,
-    });
-
-    return {
-      id: user._id,
-      tenantId: user.tenantId,
-      email: user.email,
-      role: user.role || 'USER', // Distingue entre USER y ADMIN
+    const user = {
+      tenantId: payload.tenantId,
+      email: payload.email,
+      role: payload.role || 'USER', // Distingue entre USER y ADMIN
     };
+
+    this.logger.log('Usuario validado:', user);
+    return user;
   }
 }
