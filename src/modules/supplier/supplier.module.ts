45d209dfb1eeccc1cc6eb0ba274173349fde210f
Bacze12 <brunocc.0205@gmail.com>
2025-02-15 11:45:52 -0300

    Add SupplierRepository to SupplierModule providers

--- src/modules/supplier/supplier.module.ts
+++ src/modules/supplier/supplier.module.ts
@@ -3,11 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Supplier, SupplierSchema } from './supplier.schema';
 import { SupplierService } from './supplier.service';
 import { SuppliersController } from './supplier.controller';
+import { SupplierRepository } from './repositories/supplier.repository';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Supplier.name, schema: SupplierSchema }])],
   controllers: [SuppliersController],
-  providers: [SupplierService],
+  providers: [SupplierService, SupplierRepository],
   exports: [SupplierService],
 })
 export class SupplierModule {}
